52b19cb1e663c1577a2cd993fcb69e449d7ecb6a
ivanpedroc <122410036+ivanpedroc@users.noreply.github.com>
2023-01-14 04:56:32 -0300

    Aula 06
    Loops e Arrays

---
+++ script.js
@@ -0,0 +1,11 @@
+var saida = document.getElementById("saida");
+var imprimir = "";
+
+var semana = ["Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sabado"];
+
+for(var i = 0; i < semana.length; i++){
+    imprimir += semana[i] + "<br>";
+}
+
+saida.innerHTML = imprimir;
+
